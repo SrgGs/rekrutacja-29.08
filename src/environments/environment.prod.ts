0eb1b5186323a4a43712043d509c56f6142ba597
Sergiusz Gieszczyk <s.gieszczyk@gmail.com>
2022-08-29 22:05:37 +0200

    init datebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "@env/environment.interface";
+
+export const environment:Environment = {
+  production: true,
+  backendUrl: 'http://localhost:3000/'
 };
